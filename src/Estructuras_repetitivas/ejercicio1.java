7744638870377f3e89372543a085438ccfffb443
unknown <knameless27@gmail.com>
2020-12-22 12:29:07 -0500

    Estructuras repetitivas, ejercicio vacaciones

---
+++ src/Estructuras_repetitivas/ejercicio1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Estructuras_repetitivas;
+
+/**
+ * Una clínica de maternidad necesita un sistema para determinar cuántos bebés han nacido recientemente
+ * Dicho sistema solicita al usuario capturar la cantidad de nacimientos (N)
+ * el nombre de cada bebé y su sexo (implemente un ciclo do-while para validar la captura
+ * siendo M para masculino y F para femenino). Al final, debe imprimir la cantidad de bebés de cada sexo.
+ * 
+ * @author Andres Camilo Gonzalez Velasquez
+ * cod: 20201187519
+ * fecha: 22/12/2020
+ */
+public class ejercicio1 {
+    public static void main(String[] args){
+        
+    }
+}
