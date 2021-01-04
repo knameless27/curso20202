06bc21c7f4b20b9129f51b07400a0ddb6720eecb
Andres Camilo Gonzalez Velasquez <>
2021-01-04 15:28:17 -0500

    se hace el primer punto y se agregan los proyectos junto con la carpeta de estructuras condicionales

---
+++ src/estructuras_ciclicas/ejercicio2.java
@@ -0,0 +1,28 @@
+package estructuras_ciclicas;
+import java.util.Scanner;
+/**
+ * Una heladería desea obtener un reporte semanal de ventas
+ * Para ello debe capturar en un sistema todas las facturas de ventas realizadas
+ * Cada factura contiene la cantidad y el sabor de las paletas vendidas
+ * Los sabores y los precios son: limón ($2500), nuez ($2800), lulo ($2600)
+ * fresa ($2000) y crema ($3000)
+ * El sistema debe imprimir la cantidad de paletas vendidas de cada sabor
+ * el valor vendido y la venta total
+ * Considere que una factura puede tener varias paletas de diferentes sabores.
+ * @author Andres Camilo Gonzalez Velasquez
+ * cod: 20201187519
+ * fecha: 04/01/2021
+ */
+public class ejercicio2 {
+    public static void main(String[] args) {
+        int li = 2500;
+        int nu = 2800;
+        int lu = 2600;
+        int fr = 2000;
+        int cr = 3000;
+        Scanner num = new Scanner(System.in);
+        System.out.println("Digite el numero de paletas vendidas esta semana: ");
+        int venp = num.nextInt();
+        
+    }
+}
