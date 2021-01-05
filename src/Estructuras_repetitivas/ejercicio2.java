a3b65badf89f88cbece1bae9aff917ef4aff545f
Andres Camilo Gonzalez Velasquez <>
2021-01-05 05:21:58 -0500

    hice el ejercicio 3 y corregi unos errores

--- src/Estructuras_repetitivas/ejercicio2.java
+++ src/Estructuras_repetitivas/ejercicio2.java
@@ -1,14 +1,67 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Estructuras_repetitivas;
 
+import java.util.Scanner;
+
 /**
+ * Una heladería desea obtener un reporte semanal de ventas Para ello debe
+ * capturar en un sistema todas las facturas de ventas realizadas Cada factura
+ * contiene la cantidad y el sabor de las paletas vendidas Los sabores y los
+ * precios son: limón ($2500), nuez ($2800), lulo ($2600) fresa ($2000) y crema
+ * ($3000) El sistema debe imprimir la cantidad de paletas vendidas de cada
+ * sabor el valor vendido y la venta total Considere que una factura puede tener
+ * varias paletas de diferentes sabores.
  *
- * @author EQUIPO
+ * @author Andres Camilo Gonzalez Velasquez cod: 20201187519 fecha: 04/01/2021
  */
 public class ejercicio2 {
-    
+
+    public static void main(String[] args) {
+        int li = 2500;
+        int nu = 2800;
+        int lu = 2600;
+        int fr = 2000;
+        int cr = 3000;
+        Scanner num = new Scanner(System.in);
+        Scanner lim = new Scanner(System.in);
+        Scanner nue = new Scanner(System.in);
+        Scanner lul = new Scanner(System.in);
+        Scanner fre = new Scanner(System.in);
+        Scanner cre = new Scanner(System.in);
+        System.out.println("Digite el numero de paletas vendidas esta semana: ");
+        int venp = num.nextInt();
+        int i;
+        for (i = 01; i <= venp; i++) {
+            System.out.println("cuantas paletas de limon vendio: ");
+            int limo = lim.nextInt();
+            System.out.println("cuantas paletas de nuez vendio: ");
+            int nuez = nue.nextInt();
+            System.out.println("cuantas paletas de lulo vendio: ");
+            int lulo = lul.nextInt();
+            System.out.println("cuantas paletas de fresa vendio: ");
+            int fres = fre.nextInt();
+            System.out.println("cuantas paletas de crema vendio: ");
+            int crem = cre.nextInt();
+            int sum = limo + nuez + lulo + fres + crem;
+            if (sum == venp) {
+                System.out.println("\n\n\n\n\n\n\n");
+                System.out.println("________________________________");
+                System.out.println("        -Reporte semanal-");
+                System.out.println("-Paletas de limon vendidas: " + limo +", total: "+(limo * li));
+                System.out.println("-Paletas de nuez vendidas: "+ nuez +", total: "+(nuez * nu));
+                System.out.println("-Paletas de lulo vendidas: " + lulo+", total: "+(lulo * lu));
+                System.out.println("-Paletas de fresa vendidas: " + fres+", total: "+(fres * fr));
+                System.out.println("-Paletas de crema vendidas: " + crem+", total: "+(crem * cr));
+                System.out.println("");
+                int total1 = limo*li;
+                int total2 = nuez * nu;
+                int total3 = lulo * lu;
+                int total4 = fres * fr;
+                int total5 = crem * cr;
+                System.out.println("Total de paletas vendidas: " + venp+", dinero total: "+(total1+total2+total3+total4+total5));
+                i=venp+1;
+            } else {
+                System.out.println("Numero de ventas erroneo!");
+            }
+        }
+    }
 }
--- src/Estructuras_repetitivas/ejercicio3.java
+++ src/Estructuras_repetitivas/ejercicio3.java
@@ -1,14 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Estructuras_repetitivas;
-
+import java.util.Scanner;
 /**
- *
- * @author EQUIPO
+ * 3.	El sistema meteorológico local requiere un sistema para capturar la 
+ * temperatura promedio de cada día de la semana e imprimir el nombre del día 
+ * y un mensaje de la percepción de la temperatura de acuerdo 
+ * con el valor capturado según la siguiente tabla:
+
+Temperatura	Mensaje
+Menor o igual que cero	Congelante
+1 a 10°C	Muy frío
+11 a 20°C	Frío
+21 a 24°C	Templado
+25 a 29°C	Agradable
+30 a 35°C	Caliente
+36°C o más	Muy caliente
+
+ * @author Andres Camilo Gonzalez Velasquez
+ * cod: 20201187519
+ * fecha: 05/01/2021
  */
 public class ejercicio3 {
-    
+    public static void main(String[] args) {
+        Scanner ques = new Scanner(System.in);
+        int i = 1;
+        int [] tempd = new int[8];
+        String [] dia = {"a","lunes","martes","miercoles","jueves","viernes","sabado","domingo"};
+        for(i=1;i<=7;i++)
+        {
+            System.out.println("Digite la temperatura promedio del dia "+dia[i]);
+            int temp = ques.nextInt();
+            tempd[i]=temp;
+            
+        }
+        for (i = 1; i <= 7; i++) {
+            if (tempd[i] <= 0) {
+                System.out.println("La temperatura del dia " + dia[i] + " es congelante");
+            } else if (tempd[i] >= 1 && tempd[i] <= 10) {
+                System.out.println("La temperatura del dia " + dia[i] + " es muy frio");
+            } else if (tempd[i] >= 11 && tempd[i] <= 20) {
+                System.out.println("La temperatura del dia " + dia[i] + " es frio");
+            } else if (tempd[i] >= 21 && tempd[i] <= 24) {
+                System.out.println("La temperatura del dia " + dia[i] + " es templado");
+            } else if (tempd[i] >= 25 && tempd[i] <= 29) {
+                System.out.println("La temperatura del dia " + dia[i] + " es agradable");
+            } else if (tempd[i] >= 30 && tempd[i] <= 35) {
+                System.out.println("La temperatura del dia " + dia[i] + " es caliente");
+            } else if (tempd[i] >= 36) {
+                System.out.println("La temperatura del dia " + dia[i] + " es muy caliente");
+            }
+        }
+    }
 }
